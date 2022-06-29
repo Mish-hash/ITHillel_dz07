3cc40cbeda844666ba2d683f0ba8afea2c96bb81
mish-hash <poluektov23@gmail.com>
2022-06-29 19:47:57 +0300

    1. Create array frameworks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,17 @@
+/*
+1) Создайте массив frameworks со значениями: ‘AngularJS’, ‘jQuery’
+a. Добавьте в начало массива значение ‘Backbone.js’
+b. Добавьте в конец массива значения ‘ReactJS’ и ‘Vue.js’
+c. Добавьте в массив значение ‘CommonJS’ вторым элементом
+d. Найдите и удалите из массива значение ‘jQuery’ и выведите его в консоль со словами “Это здесь лишнее”
+*/
 
+const frameworks = Array('AngularJS', 'jQuery');
+frameworks.unshift('Backbone.js');
+frameworks.push('ReactJS', 'Vue.js');
+frameworks.splice(2, 0, 'CommonJS');
+
+frameworks.forEach((elem, index, array)=>{
+    elem === 'jQuery' ? console.log(array.splice(index, 1) + ' -- Это здесь лишнее') : null;
+});
 
